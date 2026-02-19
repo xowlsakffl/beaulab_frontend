8e219051da569b645f894557b91ce68b9a9e52b9
msan <50791439+xowlsakffl@users.noreply.github.com>
2026-02-19 15:32:26 +0900

    feat: support role and permission based sidebar menus

--- packages/ui-admin/src/layouts/index.ts
+++ packages/ui-admin/src/layouts/index.ts
@@ -1,4 +1,5 @@
 export { AppHeader } from "./AppHeader";
 export { AppSidebar } from "./AppSidebar";
+export type { SidebarNavItem, SidebarNavSubItem } from "./AppSidebar";
 export { Backdrop } from "./Backdrop";
 export { SidebarWidget } from "./SidebarWidget";
