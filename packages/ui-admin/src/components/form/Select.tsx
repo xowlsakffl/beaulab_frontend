f524170daf9bc6e4c839273d38c8afd56db5db2c
xowlsakffl <xowlsakffl@naver.com>
2026-06-05 17:36:04 +0900

    feat: 병의원 상세, 수정

--- packages/ui-admin/src/components/form/Select.tsx
+++ packages/ui-admin/src/components/form/Select.tsx
@@ -25,7 +25,7 @@ export const Select: React.FC<SelectProps> = ({
   options,
   id,
   name,
-  placeholder = "Select an option",
+  placeholder = "선택",
   showPlaceholderOption = true,
   onChange,
   className = "",
